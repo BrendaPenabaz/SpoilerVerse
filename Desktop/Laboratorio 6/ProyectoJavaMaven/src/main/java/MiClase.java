0ced5229dc74606a730eba71c35ef00043b030e7
Brenda Penabaz <brendapenabaz@gmail.com>
2021-07-15 10:59:11 -0500

    Nuevos cambios en MiClase

---
+++ Desktop/Laboratorio 6/ProyectoJavaMaven/src/main/java/MiClase.java
@@ -0,0 +1,6 @@
+public class MiClase {
+    public static void main(String args[]) {
+        System.out.println("Este es un proyecto Java con Maven");
+        System.out.println("Nueva linea de codigo de prueba");
+    }
+}
